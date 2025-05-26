4a20e1b1600fb1e802d8700d7a3decb043ce7f9e
suzukikoei <koeich6@gmail.com>
2025-05-26 10:46:21 +0900

    球団コントローラの作成

---
+++ src/main/java/com/example/controller/TeamController.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import com.example.domain.Team;
+import com.example.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * 球団情報を操作するコントローラ
+ */
+@Controller
+@RequestMapping("/teams")
+public class TeamController {
+    @Autowired
+    private TeamService service;
+
+    @GetMapping("")
+    public String index(Model model){
+        List<Team> teams = service.getAllTeams();
+        model.addAttribute("teams", teams);
+        return "team-index";
+    }
+
+    @PostMapping("/showDetail")
+    public String showDetail(String id){
+        Integer team_id = Integer.parseInt(id);
+        Team team = service.getTeamInfo(team_id);
+        return "teams-info";
+    }
+}
