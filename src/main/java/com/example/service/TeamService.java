113f8a356abcba562724562bc1e8a04743ed9b48
suzukikoei <koeich6@gmail.com>
2025-05-26 10:34:40 +0900

    球団サービスの新規作成

---
+++ src/main/java/com/example/service/TeamService.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import com.example.domain.Team;
+import com.example.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 球団情報の業務処理を行うサービス.
+ */
+@Service
+@Transactional
+public class TeamService {
+    @Autowired
+    private TeamRepository repository;
+
+    public List<Team> showAllTeams(){
+        return repository.findAll();
+    }
+
+    public Team showTeam(Integer id){
+        return repository.findById(id);
+    }
+
+}
